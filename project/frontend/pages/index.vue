23951ff680ec8a1f5f07e4bbc6fdaf18392c54c8
afadel151 <af8356207@gmail.com>
2025-06-22 21:38:23 +0100

    setup index page for chatbot

--- project/frontend/pages/index.vue
+++ project/frontend/pages/index.vue
@@ -1,11 +1,19 @@
-<script setup>
+<script setup lang="ts">
 import 'highlight.js/styles/monokai.css';
+import type { ChatOutput } from '~/shared/types';
 const text = `# Hello World! **she**`
+const outputs = ref<ChatOutput[]>([])
+
+
 </script>
 
 
 <template>
-    <div class="container pt-10 pb-32">
-
+    <div class="container pt-10 pb-32 min-h-screen"
+        :class="outputs.length === 0 ? 'flex items-center justify-center' : ''">
+    >
+        <div class="w-full">
+            <h1 v-if="outputs.length === 0">What do you want to know</h1>
+        </div>
     </div>
 </template>
