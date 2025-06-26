aaa973c7d267c1b7074d4c09827928ab6a83054d
afadel151 <af8356207@gmail.com>
2025-06-26 14:06:47 +0200

    created CustomAgent

--- project/api/agent_tools.py
+++ project/api/agent_tools.py
@@ -1,10 +1,34 @@
 # this file contains tools for our Agent to use.
-from agent import Article
+import asyncio
 from langchain_core.tools import tool
 from dotenv import load_dotenv
 import aiohttp
 load_dotenv()
 import os
+
+
+from pydantic import BaseModel
+class Article(BaseModel):
+    """
+    Used for parsing serpapi results for articles.
+    This is used to extract the title, source, link, and snippet from the
+    serpapi results for articles.
+    """
+    title: str
+    source: str
+    link: str
+    snippet: str
+
+    @classmethod
+    def from_serpapi_result(cls, result: dict) -> "Article":
+        return cls(
+            title=result["title"],
+            source=result["source"],
+            link=result["link"],
+            snippet=result["snippet"],
+        )
+        
+        
 @tool
 async def add(x: float, y: float) -> float:
     """Add 'x' and 'y'."""
@@ -29,7 +53,7 @@ async def subtract(x: float, y: float) -> float:
 async def serpapi(query: str) -> list[Article]:
     """Use this tool to search the web."""
     params = {
-        "api_key": os.environ("SERPAPI_API_KEY"),
+        "api_key":os.getenv("SERPAPI_API_KEY"),
         "engine": "google",
         "q": query,
     }
@@ -40,3 +64,10 @@ async def serpapi(query: str) -> list[Article]:
         ) as response:
             results = await response.json()
     return [Article.from_serpapi_result(result) for result in results["organic_results"]]
+
+
+
+@tool
+async def final_answer(answer: str, tools_used: list[str]) -> dict[str, str | list[str]]:
+    """Use this tool to provide a final answer to the user."""
+    return {"answer": answer, "tools_used": tools_used}
---
+++ project/api/messages.py
@@ -0,0 +1,46 @@
+import asyncio
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain_core.messages import ToolMessage, AIMessage
+
+
+class QueueCallbackHandler(AsyncCallbackHandler):
+    def __init__(self, queue: asyncio.Queue):
+        self.queue = queue
+        self.final_answer_seen = False
+
+    async def __aiter__(self):
+        while True:
+            if self.queue.empty():
+                await asyncio.sleep(0.1)
+                continue
+            token_or_done = await self.queue.get()
+            if token_or_done == "<<DONE>>":
+                return
+            if token_or_done:
+                yield token_or_done
+
+    async def on_llm_new_token(self, *args, **kwargs) -> None:
+        chunk = kwargs.get("chunk")
+        if (
+            chunk
+            and chunk.message.additional_kwargs.get("tool_calls")
+            and chunk.message.additional_kwargs["tool_calls"][0]["function"]["name"] == "final_answer"
+        ):
+            self.final_answer_seen = True
+        self.queue.put_nowait(kwargs.get("chunk"))
+
+    async def on_llm_end(self, *args, **kwargs) -> None:
+        if self.final_answer_seen:
+            self.queue.put_nowait("<<DONE>>")
+        else:
+            self.queue.put_nowait("<<STEP_END>>")
+
+
+async def execute_tool(tool_call: AIMessage, name2tool: dict) -> ToolMessage:
+    tool_name = tool_call.tool_calls[0]["name"]
+    tool_args = tool_call.tool_calls[0]["args"]
+    tool_out = await name2tool[tool_name](**tool_args)
+    return ToolMessage(
+        content=f"{tool_out}",
+        tool_call_id=tool_call.tool_calls[0]["id"]
+    )
