22b499c59fae379de01962de8444cb34af752d81
afadel151 <af8356207@gmail.com>
2025-06-26 13:26:01 +0200

    created tools

--- project/api/agent.py
+++ project/api/agent.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 import asyncio
 import aiohttp
-
+from pydantic import BaseModel, SecretStr
 
 from langchain.callbacks.base import AsyncCallbackHandler
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, ToolMessage
@@ -12,4 +12,45 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import ConfigurableField
 from langchain_core.tools import tool
 
-llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai").configurable_fields(
+    callbacks=ConfigurableField(
+        id="callbacks",
+        name="callbacks",
+        description="A list of callbacks to use for streaming",
+    )
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", (
+        "You're a helpful assistant. When answering a user's question "
+        "you should first use one of the tools provided. After using a "
+        "tool the tool output will be provided back to you. When you have "
+        "all the information you need, you MUST use the final_answer tool "
+        "to provide a final answer to the user. Use tools to answer the "
+        "user's CURRENT question, not previous questions."
+    )),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human", "{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad"),
+])
+
+class Article(BaseModel):
+    """
+    Used for parsing serpapi results for articles.
+    This is used to extract the title, source, link, and snippet from the
+    serpapi results for articles.
+    """
+    title: str
+    source: str
+    link: str
+    snippet: str
+
+    @classmethod
+    def from_serpapi_result(cls, result: dict) -> "Article":
+        return cls(
+            title=result["title"],
+            source=result["source"],
+            link=result["link"],
+            snippet=result["snippet"],
+        )
+        
---
+++ project/api/tools.py
@@ -0,0 +1,42 @@
+# this file contains tools for our Agent to use.
+from agent import Article
+from langchain_core.tools import tool
+from dotenv import load_dotenv
+import aiohttp
+load_dotenv()
+import os
+@tool
+async def add(x: float, y: float) -> float:
+    """Add 'x' and 'y'."""
+    return x + y
+
+@tool
+async def multiply(x: float, y: float) -> float:
+    """Multiply 'x' and 'y'."""
+    return x * y
+
+@tool
+async def exponential(x: float, y: float) -> float:
+    """Raise 'x' to the power of 'y'."""
+    return x ** y
+
+@tool
+async def subtract(x: float, y: float) -> float:
+    """Subtract 'x' from 'y'."""
+    return y - x
+
+@tool
+async def serpapi(query: str) -> list[Article]:
+    """Use this tool to search the web."""
+    params = {
+        "api_key": os.environ("SERPAPI_API_KEY"),
+        "engine": "google",
+        "q": query,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://serpapi.com/search",
+            params=params
+        ) as response:
+            results = await response.json()
+    return [Article.from_serpapi_result(result) for result in results["organic_results"]]
