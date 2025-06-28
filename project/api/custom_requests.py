69c54343b40480a46f7fa24543a9386202b8eea2
afadel151 <af8356207@gmail.com>
2025-06-28 22:13:06 +0200

    created custom request classes

---
+++ project/api/custom_requests.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class InvokeRequest(BaseModel):
+    content: str
+    conversation_id: str
+class CreateConversationRequest(BaseModel):
+    title: str = "New Conversation"
+    
+class GetConversationRequest(BaseModel):
+    conversation_id: str
+    
